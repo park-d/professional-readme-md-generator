7c759344c05cf489b704b3d7a7426c74c641c709
park-d <54589582+park-d@users.noreply.github.com>
2022-09-22 15:25:57 -0500

    added the questions that will come up in the inquirer prompt

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your project title?*',
+    'Provide a short description of your project.*',
+    'What are the steps required to install your project?',
+    'Provide instructions and examples of use.',
+    'List your collaborators, if any, with links to their GitHub profiles.',
+    'Choose a license',
+    'Provide contribution guidelines for your project:',
+    'Provide test instructions and examples for your project:',
+    'What is your GitHub username?*',
+    'What is your email address?*',
+    'Provide instructions how to reach you with additional questions.'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
