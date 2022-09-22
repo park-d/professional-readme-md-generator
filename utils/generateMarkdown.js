70ad9a012bf4f76683099e587807ed37493c4a70
park-d <54589582+park-d@users.noreply.github.com>
2022-09-22 15:34:46 -0500

    added the links to the badge image in a switch statement

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,27 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case "GNU AGPL v3":
+      return `(https://img.shields.io/badge/License-AGPL_v3-blue.svg)]`;
+    case "GNU GPL v3":
+      return `(https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+    case "GNU LGPL v3":
+      return `(https://img.shields.io/badge/License-LGPL_v3-blue.svg)`;
+    case "Mozilla Public License 2.0":
+      return `(https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`;
+    case "Apache License 2.0":
+      return `(https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+    case "The MIT License":
+      return `(https://img.shields.io/badge/License-MIT-yellow.svg)`;
+    case "Boost Software License 1.0":
+      return `(https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`;
+    case "The Unlicense":
+      return `(https://img.shields.io/badge/license-Unlicense-blue.svg)`;
+    default:
+      return;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
