711b15fbda5f7c95c1e541b869d7cb45a6627870
park-d <54589582+park-d@users.noreply.github.com>
2022-09-22 15:24:07 -0500

    added the initial layout for the README.md file in the generateMarkdown.js file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,33 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+    
+## Installation
+
+## Usage 
+
+## Credits
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
+
+- GitHub: 
+- Email:
 `;
 }
 
